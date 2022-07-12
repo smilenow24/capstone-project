f09d283f13f8d099fedd90d7279746a3fad69c19
Jeremias Erbs <jerry@badabam.de>
2022-07-12 14:03:16 +0200

    All the changes

--- src/App.js
+++ src/App.js
@@ -1,132 +1,48 @@
 import {nanoid} from 'nanoid';
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import styled from 'styled-components';
 
 import BarChart from './components/BarChart';
 import CategoryButton from './components/CategoryButton';
 import Header from './components/Header';
+import InfoBoard from './components/InfoBoard.js';
 import InputDataDialog from './components/InputDataDialog.js';
 import LineChart from './components/LineChart';
 import ReturnButton from './components/ReturnButton';
-import {initialInputData} from './db';
-import {messages} from './db';
+import {initialInputData, messages} from './db';
+import getTotalConsumption from './services/getTotalConsumption.js';
 
 export default function App() {
-  const [inputs, setInputs] = useState(initialInputData);
-  const [showMessage, setShowMessage] = useState(messages[0].text);
-  const [toggle, setToggle] = useState(true);
-  const [totalConsumption, setTotalConsumption] = useState({});
+  const [energyConsumptionHistory, setEnergyConsumptionHistory] = useState(initialInputData);
+  const [messageText, setMessageText] = useState(messages[0].text);
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const totalConsumption = getTotalConsumption(energyConsumptionHistory);
   const oldInputLength = initialInputData.length;
-  const actualDate = new Date();
-  const formattedActualDate = actualDate.toLocaleDateString('en-GB', {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  });
-  const differenceDate = inputs[0].date.getTime() - inputs[inputs.length - 1].date.getTime();
-  const differenceDays = Math.round(differenceDate / (24 * 3600 * 1000)) + 1;
-  const totalBudget = 30000;
   const dailyTotalBudget = 1000;
-  const saldoIncrease = inputs[0].increase - dailyTotalBudget;
-
-  useEffect(() => {
-    function handleCalc() {
-      const total = inputs.map(input => input.increase).reduce((a, b) => a + b, 0);
-      const averageIncrease = total / differenceDays;
-      const averageIncreaseRounded = Math.round(averageIncrease);
-      setTotalConsumption({total, averageIncreaseRounded});
-    }
-    handleCalc();
-  }, [inputs, differenceDays]);
-
-  function updateChart() {
-    const chartInputData = {
-      labels: inputs.reverse().map(input =>
-        input.date.toLocaleDateString('en-GB', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit',
-        })
-      ),
-      datasets: [
-        {
-          label: 'Consumption Data',
-          data: inputs.map(input => input.increase),
-          borderColor: inputs.reverse()[0].increase > dailyTotalBudget ? 'red' : 'green',
-          borderWidth: '2',
-        },
-      ],
-    };
-    return chartInputData;
-  }
-
-  function calcBudget() {
-    const restBudget = totalBudget - totalConsumption.total;
-    return restBudget;
-  }
-
-  function updateEnergyConsumption(inputEnergyConsumptionValue) {
-    const newInput = {
-      id: nanoid(),
-      date: actualDate,
-      value: Number(inputEnergyConsumptionValue),
-      increase: inputEnergyConsumptionValue - inputs[0].value,
-    };
-
-    if (newInput.value >= inputs[0].value) {
-      setInputs([newInput, ...inputs]);
-      if (oldInputLength !== inputs.length) {
-        setShowMessage(messages.text.id[2]);
-      } else {
-        setShowMessage(messages[2].text);
-      }
-      if (inputs[0].increase > dailyTotalBudget) {
-        setShowMessage(messages[2].text);
-      }
-    } else {
-      setShowMessage('please input > or = ' + inputs[0].value);
-    }
-  }
+  const chartInputData = updateChart();
 
   return (
     <>
-      <Header showMessage={showMessage} />
-      {!toggle && <ReturnButton onReturn={() => setToggle(!toggle)} />}
+      <Header showMessage={messageText} />
+      {currentPage === 'electricity' && <ReturnButton onReturn={() => setCurrentPage(!currentPage)} />}
       <MainHeading>Energy-Budget-App</MainHeading>
-      {toggle && (
+      {currentPage === 'home' && (
         <CategoryButton
-          lastInputValue={inputs[0].value}
-          lastInputIncrease={inputs[0].increase}
-          onSelect={() => setToggle(!toggle)}
+          lastInputValue={energyConsumptionHistory[0].value}
+          lastInputIncrease={energyConsumptionHistory[0].increase}
+          onSelect={() => setCurrentPage(!currentPage)}
         />
       )}
-      {!toggle && (
+      {currentPage === 'electricity' && (
         <MainContainer>
-          <InfoBoard>
-            <h2>{formattedActualDate}</h2>
-            <ul>
-              <li>
-                total entries: <b>{inputs.length}</b>
-              </li>
-              <li>
-                total consumption: <b>{totalConsumption.total.toLocaleString('de-DE')}</b> watt/h
-              </li>
-              <li>
-                total budget: <b>30000</b> - rest budget: <b>{calcBudget()}</b>
-              </li>
-              <li style={{color: totalConsumption.averageIncreaseRounded > dailyTotalBudget ? 'red' : '#2aff00'}}>
-                daily average increase: <b>{totalConsumption.averageIncreaseRounded.toLocaleString('de-DE')}</b> watt/h
-              </li>
-              <li>
-                accepted increase value: <b>{dailyTotalBudget} watt/h</b>
-              </li>
-              <li style={{color: saldoIncrease > dailyTotalBudget ? 'red' : '#2aff00'}}>
-                saldo increase value: <b>{saldoIncrease} watt/h</b>
-              </li>
-            </ul>
-          </InfoBoard>
+          <InfoBoard
+            energyConsumptionHistory={energyConsumptionHistory}
+            dailyTotalBudget={dailyTotalBudget}
+            totalConsumption={totalConsumption}
+          />
           <InputDataList role="list">
-            {inputs.map(({date, value, id, increase}) => (
+            {energyConsumptionHistory.map(({date, value, id, increase}) => (
               <li key={id}>
                 {date.toLocaleDateString('en-GB', {
                   year: 'numeric',
@@ -136,14 +52,58 @@ export default function App() {
                 - {value.toLocaleString('de-DE')} watt/h - increase: {increase.toLocaleString('de-DE')}
               </li>
             ))}
-            <LineChart lineChartData={updateChart()} />
-            <BarChart barChartData={updateChart()} />
+            <LineChart lineChartData={chartInputData} />
+            <BarChart barChartData={chartInputData} />
           </InputDataList>
           <InputDataDialog updateEnergyConsumption={updateEnergyConsumption} />
         </MainContainer>
       )}
     </>
   );
+
+  function updateChart() {
+    const chartInputData = {
+      labels: energyConsumptionHistory.reverse().map(input =>
+        input.date.toLocaleDateString('en-GB', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+        })
+      ),
+      datasets: [
+        {
+          label: 'Consumption Data',
+          data: energyConsumptionHistory.map(input => input.increase),
+          borderColor: energyConsumptionHistory.reverse()[0].increase > dailyTotalBudget ? 'red' : 'green',
+          borderWidth: '2',
+        },
+      ],
+    };
+    return chartInputData;
+  }
+
+  function updateEnergyConsumption(inputEnergyConsumptionValue) {
+    const newInput = {
+      id: nanoid(),
+      date: new Date(),
+      value: Number(inputEnergyConsumptionValue),
+      increase: inputEnergyConsumptionValue - energyConsumptionHistory[0].value,
+    };
+
+    if (newInput.value >= energyConsumptionHistory[0].value) {
+      setEnergyConsumptionHistory([newInput, ...energyConsumptionHistory]);
+      if (oldInputLength !== energyConsumptionHistory.length) {
+        setMessageText(messages.success);
+      } else {
+        setMessageText(messages.dataNeeded);
+      }
+      if (energyConsumptionHistory[0].increase > dailyTotalBudget) {
+        setMessageText(messages[2].text);
+      }
+    } else {
+      setMessageText('please input > or = ' + energyConsumptionHistory[0].value);
+    }
+  }
 }
 
 const MainHeading = styled.h1`
@@ -174,34 +134,5 @@ const InputDataList = styled.ul`
     font-weight: bolder;
     font-size: 15px;
     border-bottom: 1px solid;
-  }
-`;
-
-const InfoBoard = styled.section`
-  display: flex;
-  background-color: #053f72;
-  border-radius: 20px;
-
-  h2 {
-    color: white;
-    padding: 0.1vh 2vh 0.1vh 2vh;
-    font-size: medium;
-  }
-
-  ul {
-    width: 40vh;
-    list-style: none;
-    padding: 0.1vh;
-    border-bottom: none;
-  }
-
-  li {
-    color: white;
-    font-size: 1rem;
-    font-weight: 500;
-  }
-
-  b {
-    font-size: medium;
   }
 `;
